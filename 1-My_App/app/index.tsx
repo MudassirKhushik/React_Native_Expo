9482e7bb0fd7fbc95df3e3e8e542788fb3da46a8
Mudassir Khushik <150449494+MudassirKhushik@users.noreply.github.com>
2025-07-01 13:33:02 +0000

    My Expo app

---
+++ 1-My_App/app/index.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from "react-native"
+
+const Index = () => {
+return (
+  <View style={{ flex: 1,
+    backgroundColor: "red",
+    justifyContent: "center",
+    alignItems: "center"
+   }}>
+    <Text style={{ fontSize: 30, fontWeight: 700, color: "white" }}>Hello Mudassir Hi Mudassir</Text>
+  </View>
+);
+};
+
+export default Index
